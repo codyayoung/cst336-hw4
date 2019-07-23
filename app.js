8120e6c9f42fd14873f0a43aff3fd70785b924e5
Cody Young <cyoung1623@gmail.com>
2019-07-22 19:18:41 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    res.render("index.html");
+});
+
+//Server listener
+//app.listen("8081", "127.0.0.1", function() {
+//    console.log("Express Server is running...");
+//});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express server...");
+});
+
