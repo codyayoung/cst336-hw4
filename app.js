fc21dbafa74aef43e07eaa12cdddc77173d2fa81
Cody Young <cyoung1623@gmail.com>
2019-07-22 21:10:56 -0700

    Heroku deployment

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-//Faker variables
-var eaxHex = require('faker');
-var ecxHex = require('faker');
+var faker = require('faker');
 
 const express = require("express");
 const app = express();
@@ -12,13 +10,13 @@ app.get("/", function(req, res) {
 });
 
 //Server listener
-app.listen("8081", "127.0.0.1", function() {
-    console.log("Express Server is running...");
-});
-
-//app.listen(process.env.PORT, process.env.IP, function() {
-//    console.log("Running Express server...");
+//app.listen("8081", "127.0.0.1", function() {
+//    console.log("Express Server is running...");
 //});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express server...");
+});
 
 app.get("/", function(req, res) {
     res.render("index.html");
@@ -28,12 +26,14 @@ app.get("/registers", function(req, res) {
     res.render("registers.html");
 });
 
-app.get("/instructions", function(req, res) {
-    res.render("instructions.html");
+app.get("/instructions", function(req, res) { 
+    var eaxRandom = faker.random.number(200); 			
+
+    var eaxResult = "movl" + " " + "$"+ eaxRandom + "," + " " + "%eax";
+
+    res.render("instructions.ejs", {"eaxResult":eaxResult});
 });
 
 app.get("/history", function(req, res) {
     res.render("history.html");
 });
-
-
